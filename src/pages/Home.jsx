8873f9e23120ccfa4cd6978a5c08e5634e646471
Sifat-Ikram <sifatikram@gmail.com>
2024-02-01 15:23:42 +0600

    webpage completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,12 +20,12 @@ const Home = () => {
       <h1 className='font-bold text-3xl'>Featured Shows</h1>
       <div className='mt-16 grid grid-cols-1 gap-8 lg:grid-cols-3 md:grid-cols-2'>
         {
-          data.map(show => <div className="card w-80 bg-base-200 shadow-xl" key={show.score}>
+          data.map(show => <div className="card w-72 bg-base-200 shadow-xl" key={show.show.id}>
           <figure><img src={show.show.image?.medium} alt={show.show.name} className='h-60' /></figure>
           <div className="card-body">
             <h2 className="card-title">{show.show.name}</h2>
             <h1 className='flex gap-2'>
-            <span className='font-semibold'>Genres:</span> {show.show.genres.map(type => <h1 key={type}>{type}</h1>)}
+            <span className='font-semibold'>Genres:</span> {show.show.genres.map(type => <span key={type}>{type}</span>)}
             </h1>
             <h1><span className='font-semibold'>Premiered date:</span> {show.show.premiered}</h1>
             <p><span className='font-semibold'>IMDB:</span> {show.show.rating.average}</p>
--- src/pages/Summary.jsx
+++ src/pages/Summary.jsx
@@ -19,7 +19,28 @@ const Summary = () => {
     const showDetails = filteredItem[0].show;
     console.log(showDetails);
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const name = form.get('name');
+        const country = form.get('country');
+        const date = form.get('date');
+        const language = form.get('language');
+        const time = form.get('time');
 
+        const showData = {
+            name,
+            country,
+            date,
+            language,
+            time,
+        };
+
+        localStorage.setItem('showData', JSON.stringify(showData));
+
+        const modal = document.getElementById('my_modal_5');
+        modal.close();
+    }
 
     return (
         <div className="w-4/5 mx-auto space-y-5 my-10">
@@ -27,20 +48,79 @@ const Summary = () => {
                 <button className="btn btn-primary">Previous</button>
             </Link>
             <div className="my-20 space-y-10">
-                <h1 className="text-5xl font-extrabold text-center">Show Details</h1>
+                <h1 className="md:text-5xl text-3xl font-extrabold text-center">Show Details</h1>
                 <div className="flex flex-col justify-center items-center">
                     <img src={showDetails.image.original} alt={showDetails.name} className="h-96" />
                 </div>
                 <div className="space-y-5">
                     <h1><span className="font-bold text-xl">Show: {showDetails.name}</span></h1>
                     <h1 className='flex gap-2 items-center'>
-                        <span className='font-bold text-xl'>Genres: </span> {showDetails.genres.map(type => <h1 key={type}>{type}</h1>)}
+                        <span className='font-bold text-xl'>Genres: </span> {showDetails.genres.map(type => <div key={type}>{type}</div>)}
                     </h1>
                     <h1><span className="font-bold text-xl">Language: </span> {showDetails.language}</h1>
                     <h1><span className="font-bold text-xl">Premiered Date: </span> {showDetails.premiered}</h1>
                     <h1><span className="font-bold text-xl">Summary: </span>
                         <p className="mt-3" dangerouslySetInnerHTML={{ __html: showDetails.summary }} />
                     </h1>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary" onClick={() => document.getElementById('my_modal_5').showModal()}>book</button>
+                        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                            <div className="modal-box">
+                                <form className="card-body" onSubmit={handleSubmit}>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Show name</span>
+                                        </label>
+                                        <input type="text"
+                                            name="name"
+                                            className="input input-bordered"
+                                            defaultValue={showDetails.name} />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Country</span>
+                                        </label>
+                                        <input type="text"
+                                            name="country"
+                                            className="input input-bordered"
+                                            defaultValue={showDetails?.network?.country?.name} />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">language</span>
+                                        </label>
+                                        <input type="text"
+                                            name="language"
+                                            className="input input-bordered"
+                                            defaultValue={showDetails.language} />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Date</span>
+                                        </label>
+                                        <input type="date"
+                                            name="date"
+                                            className="input input-bordered" />
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Time</span>
+                                        </label>
+                                        <input type="text"
+                                            name="time"
+                                            className="input input-bordered"
+                                            defaultValue={showDetails.schedule.time} />
+                                    </div>
+                                    <div className="form-control mt-6">
+                                        <button type="submit" className="btn btn-primary">Confirm</button>
+                                    </div>
+                                </form>
+                            </div>
+                            <form method="dialog" className="modal-backdrop">
+                                <button>close</button>
+                            </form>
+                        </dialog>
+                    </div>
                 </div>
             </div>
         </div>
